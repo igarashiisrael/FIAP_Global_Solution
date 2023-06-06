01043fd6e58fa2a71044af0f5ba9092e9df7e8bd
RyanAquinoSantos <77982643+RyanAquinoSantos@users.noreply.github.com>
2023-06-06 19:00:21 -0300

    página 4
    minha página aqui galera

---
+++ assets/JS/scriptpage4.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({ reset: true});
+
+// rotate:{x:0, y:0, z:0} caso queira mudar o estilo da animação
+
+sr.reveal('#container_ODS1', {duration: 2000});
+
+sr.reveal('#container_ODS2', {duration: 2000});
+
+sr.reveal('#container_ODS3', {duration: 2000});
+
+sr.reveal('#container_ODS4', {duration: 2000});
