c0ab508f3b3a6ef69ab78272bb22560a8d38b1ed
Israel Igarashi <89955427+igarashiisrael@users.noreply.github.com>
2023-06-07 21:29:26 -0300

    correcao de itens

--- assets/JS/functions.js
+++ assets/JS/functions.js
@@ -1,3 +1,9 @@
+var botao = document.getElementById('btn-invest');
+var totalinvest = document.getElementById('num-invest');
+var cont = 0;
+
+botao.addEventListener('click', clicou);
+
 function validarEmail() {
   var emailInput = document.querySelector('.input-email');
   var email = emailInput.value;
@@ -23,18 +29,44 @@ function validarEmailIndex() {
 
 }
 
-var botao = document.getElementById('btn-invest');
-var totalinvest = document.getElementById('num-invest');
-var cont = 0;
-    
-botao.addEventListener('click', clicou);
-
-function clicou(){
-  if (cont!=0) {
-    botao.innerHTML = `Você já investitiu`;
+function clicou() {
+  if (cont != 0) {
+    alert("Você já faz parte dos nossos investidores! Obrigado!")
   }
   else {
     totalinvest.innerHTML = `5000`;
-    cont ++;
+    cont++;
+    botao.innerHTML = `Obrigado pelo Apoio!`;
   }
 }
+
+/* function autoCarousel() {
+  // Obtém a referência para o carrossel
+  const carousel = document.querySelector('#carouselExampleFade');
+
+  // Verifica se o carrossel existe
+  if (carousel) {
+    // Define o intervalo de tempo em milissegundos (por exemplo, 3 segundos)
+    const interval = 3000;
+
+    // Função para avançar para o próximo slide
+    const nextSlide = () => {
+      const activeSlide = carousel.querySelector('.carousel-item.active');
+      const next = activeSlide.nextElementSibling || carousel.firstElementChild;
+      activeSlide.classList.remove('active');
+      next.classList.add('active');
+    };
+
+    // Chama a função nextSlide a cada intervalo de tempo
+    setInterval(nextSlide, interval);
+  }
+}
+
+// Chama a função para iniciar o carrossel automático
+autoCarousel(); */
+
+
+
+
+
+
