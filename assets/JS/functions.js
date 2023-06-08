d2507648d0e580bfc3baa6c4bb9e89ba16bc108d
Israel Igarashi <89955427+igarashiisrael@users.noreply.github.com>
2023-06-07 23:03:43 -0300

    finalizacao do projeto

--- assets/JS/functions.js
+++ assets/JS/functions.js
@@ -1,6 +1,10 @@
 var botao = document.getElementById('btn-invest');
 var totalinvest = document.getElementById('num-invest');
 var cont = 0;
+const telefoneInput = document.querySelector('.valida-telefone');
+telefoneInput.addEventListener('input', formatarTelefone);
+
+
 
 botao.addEventListener('click', clicou);
 
@@ -26,22 +30,10 @@ function validarEmailIndex() {
   } else {
     alert("Por favor, insira um e-mail válido.");
   }
-
 }
 
-function clicou() {
-  if (cont != 0) {
-    alert("Você já faz parte dos nossos investidores! Obrigado!")
-  }
-  else {
-    totalinvest.innerHTML = `5000`;
-    cont++;
-    botao.innerHTML = `Obrigado pelo Apoio!`;
-  }
-}
-
-function validarEmail() {
-  const telefoneInput = document.querySelector('.input-telefone');
+function validarTelefone() {
+  const telefoneInput = document.querySelector('.valida-telefone');
   const telefone = telefoneInput.value.replace(/\D/g, ''); // Remove caracteres não numéricos
   const ddd = telefone.slice(0, 2);
   const numero = telefone.slice(2);
@@ -49,20 +41,13 @@ function validarEmail() {
   const telefoneFormatado = `(${ddd}) ${numero}`;
 
   if (!telefone) {
-    return; // Retorna sem exibir o alerta
+    return;
   }
-  // Validação do telefone
   if (telefone.length !== 11 || isNaN(telefone)) {
     alert('Digite um número de telefone válido!');
     return;
   } else { alert('Telefone cadastrado com sucesso!') }
-
-  // Exibe o telefone formatado no console (pode ser alterado para exibir em outro lugar, como em um elemento HTML)
-  console.log(telefoneFormatado);
 }
-
-const telefoneInput = document.querySelector('.input-telefone');
-telefoneInput.addEventListener('input', formatarTelefone);
 
 function formatarTelefone() {
   const telefone = telefoneInput.value.replace(/\D/g, ''); // Remove caracteres não numéricos
@@ -77,4 +62,16 @@ function formatarTelefone() {
   }
 
   telefoneInput.value = telefoneFormatado;
+}
+
+
+function clicou() {
+  if (cont != 0) {
+    alert("Você já faz parte dos nossos investidores! Obrigado!")
+  }
+  else {
+    totalinvest.innerHTML = `5000`;
+    cont++;
+    botao.innerHTML = `Obrigado pelo Apoio!`;
+  }
 }
